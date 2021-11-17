fa82744b6e1df095b2db103d2d7857b5b2be1e37
Dylan Neve <dylanneve1@gmail.com>
2021-11-17 09:54:07 +0000

    libnfc-nfi: Remove prebuilt

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -82,7 +82,6 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/etc/libnfc-sec-vendor.conf
 
 # Lights
