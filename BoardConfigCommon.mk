faa39bfd994aaa4f6680b17b55be0156b012c9c1
Dylan Neve <dylanneve1@gmail.com>
2021-11-17 01:13:41 +0000

    BoardConfig: Enable more hacks

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -13,8 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Hacks
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_DUP_SYSPROP := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+SELINUX_IGNORE_NEVERALLOWS := true
+RELAX_USES_LIBRARY_CHECK := true
 
 COMMON_PATH := device/samsung/universal9810-common
 
